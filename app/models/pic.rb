71ca73628cca1821f1feca0736fae425cdf8b737
zach beecher <zachbeecher@gmail.com>
2017-11-20 00:15:04 -0800

    add masonry gem and styling

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,5 +1,6 @@
 class Pic < ApplicationRecord
     belongs_to :user
 
-    has_attached_file :image, styles: lambda { |attachment| { thumb: (attachment.instance.boss? ? "300x300>" : "100x100>") } }
+    has_attached_file :image, :styles => { :medium => "300x300>" }
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
