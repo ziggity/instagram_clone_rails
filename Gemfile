477ea13f87d2c819a6220a68fd4061fd51edfdce
zach beecher <zachbeecher@gmail.com>
2017-11-19 23:26:12 -0800

    show html and more controller logic

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'haml'
 gem 'simple_form', '~> 3.2', '>= 3.2.1'
 gem 'bootstrap-sass', '~> 3.3.7'
-
+gem 'devise'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,7 +2,7 @@ class PicsController < ApplicationController
 
 
     def index
-
+        @pics = Pic.all.order("created_at DESC")
     end
 
     def new
@@ -11,11 +11,37 @@ class PicsController < ApplicationController
 
     def create
         @pic = Pic.new(pic_params)
+        if @pic.save
+            redirect_to @pic, notice: "yes it was posted!"
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @pic.update(pic_params)
+            redirect_to @pic, notice: "Congrats pic was updated!"
+        else
+            render 
+        end
+
+    def destroy
+        @pic.destroy 
+        redirect_to root_path
+        
     end
 
     private
 
     def pic_params
         params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+        @pic = Pic.find(params[:id])
     end
 end
